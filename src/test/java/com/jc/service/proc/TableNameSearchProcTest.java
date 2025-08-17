378a99bdb21da025dd9dc2bf00d8955e2441ce30
suteado90573 <suteado90573@gmail.com>
2025-08-17 16:49:12 +0900

    JUnit全テストエラーとなる問題に対応

--- src/test/java/com/jc/service/proc/TableNameSearchProcTest.java
+++ src/test/java/com/jc/service/proc/TableNameSearchProcTest.java
@@ -49,9 +49,9 @@ public class TableNameSearchProcTest extends TestBase {
 		proc.doProc(input, output, input.getString("dirPath"));
 		assertNull(output.getDb());
 		assertEquals(5, output.getRecordList("tableNameList").size());
-		assertEquals("GNR_GRP", output.getRecordList("tableNameList").get(0).get("Tables_in_db"));
-		assertEquals("GNR_KEY_VAL", output.getRecordList("tableNameList").get(1).get("Tables_in_db"));
-		assertEquals("ACCNT", output.getRecordList("tableNameList").get(2).get("Tables_in_db"));
+		assertEquals("ACCNT", output.getRecordList("tableNameList").get(0).get("Tables_in_db"));
+		assertEquals("GNR_GRP", output.getRecordList("tableNameList").get(1).get("Tables_in_db"));
+		assertEquals("GNR_KEY_VAL", output.getRecordList("tableNameList").get(2).get("Tables_in_db"));
 		assertEquals("SCR", output.getRecordList("tableNameList").get(3).get("Tables_in_db"));
 		assertEquals("SCR_ELM", output.getRecordList("tableNameList").get(4).get("Tables_in_db"));
 	}
